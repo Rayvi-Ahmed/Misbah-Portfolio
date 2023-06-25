893086bb1add60f921c75d92ba86aa40b4053e0b
Misbah Uddin <rayvi73@gmail.com>
2023-06-25 15:52:25 +0600

    banner & about section complete

--- src/Componants/About/About.jsx
+++ src/Componants/About/About.jsx
@@ -1,9 +1,78 @@
-
+import { motion } from 'framer-motion'
+import CountUp from 'react-countup';
+import { useInView } from 'react-intersection-observer';
+import { fadeIn } from '../../Varient/Variants'
 
 const About = () => {
+    const [ref, inview] = useInView({
+        threshold: 0.5,
+    })
     return (
-        <div className="py-8 lg:py-24 lg:h-screen flex items-center text-white" id="about">
-            <h1>This is About</h1>
+        <div className="py-8 lg:py-24 lg:h-screen flex items-center text-white" ref={ref} id="about">
+            <div className='flex'>
+                <div
+                    className='container mx-auto lg:flex items-center'>
+                    <motion.div
+                        variants={fadeIn('right', 0.5)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }}
+
+                        className='bg-about bg-contain bg-no-repeat flex-1 h-[640px] mix-blend-lighten bg-top'></motion.div>
+
+                    <motion.div
+                        variants={fadeIn('left', 0.5)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }}
+
+                        className='flex-1'>
+                        <h1 className='text-accent font-semibold text-4xl mb-4'>About Me,</h1>
+                        <h3 className='font-semibold text-3xl'>Im a junior front end web developer with 1 years of experiance</h3>
+                        <p className='font-semibold text-lg mb-5'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, dolore! Vero, quam aliquam? Obcaecati labore laboriosam, repellendus officiis totam magni?</p>
+
+                        <div className='flex gap-x-8'>
+                            <div>
+                                <div className='text-[40px] bg-gradient-to-r text-transparent bg-clip-text from-[#42A6E3] to-[#FF56F6] font-bold font-primary hover: from-[#FF56F6] hover:to-[#42A6E3];mb-2'>
+                                    {inview ? <CountUp
+                                        start={0} end={1} duration={3}
+                                    >
+
+                                    </CountUp> : null}+
+                                </div>
+                                <div className='font-bold text-sm tracking-[2px]'>
+                                    years of <br></br>
+                                    experience
+                                </div>
+                            </div>
+                            <div>
+                                <div className='text-[40px] bg-gradient-to-r text-transparent bg-clip-text from-[#42A6E3] to-[#FF56F6] font-bold font-primary hover: from-[#FF56F6] hover:to-[#42A6E3];mb-2'>
+                                    {inview ? <CountUp
+                                        start={0} end={9} duration={3}
+                                    >
+
+                                    </CountUp> : null}+
+                                </div>
+                                <div className='font-bold text-sm tracking-[2px]'>
+                                    My total <br></br>
+                                    Project
+                                </div>
+                            </div>
+                            <div>
+                                <div className='text-[40px] bg-gradient-to-r text-transparent bg-clip-text from-[#42A6E3] to-[#FF56F6] font-bold font-primary hover: from-[#FF56F6] hover:to-[#42A6E3];mb-2'>
+                                    {inview ? <CountUp
+                                        start={0} end={5} duration={3}
+                                    >
+
+                                    </CountUp> : null}+
+                                </div>
+                                <div className='font-bold text-sm tracking-[2px]'>
+                                    Stisfied <br></br>
+                                    Client
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </motion.div>
+
+
+                </div>
+            </div>
         </div>
     );
 };
--- src/Componants/Banner/Banner.jsx
+++ src/Componants/Banner/Banner.jsx
@@ -2,24 +2,22 @@ import { TypeAnimation } from 'react-type-animation';
 import image from '../../assets/avatar.svg'
 import { FaLinkedin, FaFacebook, FaGithub } from 'react-icons/fa';
 import { motion } from 'framer-motion'
-import { fadeIn } from './Variants'
+import { fadeIn } from '../././././../Varient/Variants'
 
 const Banner = () => {
     return (
-
-
         <div className='min-h-[85vh] lg:min-[78vh] flex items-center text-white' id="home">
             <div className='container mx-auto'>
                 <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center'>
                     <div className='flex-1 text-center lg:text-left'>
                         <motion.h1 variants={fadeIn('up', 0.3)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }}
 
-                            className='text-[50px] lg:text-[70px] font-bold leading-[0.8] mb-5 uppercase'>Misbah uddin</motion.h1>
+                            className='text-[50px] lg:text-[60px] font-bold leading-[0.8] mb-5 uppercase'>Misbah uddin</motion.h1>
                         <motion.div
                             variants={fadeIn('up', 0.3)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }}
 
 
-                            className='mb-6 text-[36px] lg:text-[60px] font-semibold uppercase leading-[1]'>
+                            className='mb-6 text-[36px] lg:text-[45px] font-semibold uppercase leading-[1]'>
                             <span className='mr-4'>Im a</span>
                             <TypeAnimation
                                 sequence={[
@@ -40,14 +38,22 @@ const Banner = () => {
                             </TypeAnimation>
                         </motion.div>
 
-                        <p className='mb-6'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure dolorum explicabo expedita repudiandae. Officia repellendus rem amet ut qui aspernatur saepe perferendis dolores sint voluptatem incidunt nihil asperiores autem enim necessitatibus, a porro nam maxime nesciunt et eos? Voluptatibus, vero?</p>
+                        <motion.p
+                            variants={fadeIn('up', 0.5)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }}
+                            className='mb-6 lg:max-w-[80%]'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure dolorum explicabo expedita repudiandae. Officia repellendus rem amet ut qui aspernatur saepe perferendis dolores sint voluptatem incidunt nihil asperiores autem enim necessitatibus, a porro nam maxime nesciunt et eos? Voluptatibus, vero?</motion.p>
 
-                        <div className='flex max-w-max gap-x-6 items-center lg:mb-6'>
+                        <motion.div
+                            variants={fadeIn('up', 0.6)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }}
+
+                            className='flex max-w-max gap-x-6 items-center lg:mb-6'>
                             <button className="btn p-2 rounded-lg">Contact Me</button>
                             <a href="#" className='text-gradient btn-link ml-4'>My Portfolio</a>
-                        </div>
+                        </motion.div>
 
-                        <div className='flex max-w-max gap-x-6 mx-auto lg:mx-0'>
+                        <motion.div
+                            variants={fadeIn('up', 0.7)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }}
+
+                            className='flex max-w-max gap-x-6 mx-auto lg:mx-0'>
                             <a href="#">
                                 <FaFacebook></FaFacebook>
                             </a>
@@ -58,12 +64,15 @@ const Banner = () => {
                                 <FaGithub></FaGithub>
                             </a>
 
-                        </div>
+                        </motion.div>
                     </div>
 
-                    <div className='hidden flex-1 lg:flex max-w-[320px] lg:max-w-[480px]'>
+                    <motion.div
+                        variants={fadeIn('down', 0.5)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }}
+
+                        className='hidden flex-1 lg:flex max-w-[320px] lg:max-w-[480px]'>
                         <img src={image} alt="" />
-                    </div>
+                    </motion.div>
                 </div>
 
             </div>
