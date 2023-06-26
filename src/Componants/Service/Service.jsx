fe48ed952deb499fe798cfd9049bbb201582db8c
Misbah Uddin <rayvi73@gmail.com>
2023-06-26 10:53:11 +0600

    completed my Services

--- src/Componants/Service/Service.jsx
+++ src/Componants/Service/Service.jsx
@@ -1,5 +1,6 @@
 import { fadeIn } from '../../Varient/Variants'
 import { motion } from 'framer-motion'
+import { FaLocationArrow } from 'react-icons/fa';
 
 const Service = () => {
     const services = [
@@ -9,17 +10,17 @@ const Service = () => {
             link: 'learn more'
         },
         {
-            name: " UI / UX design",
+            name: "PSD to HTML",
             description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, dolore! Vero, quam aliquam? Obcaecati labore laboriosam, repellendus officiis totam magni',
             link: 'learn more'
         },
         {
-            name: " UI / UX design",
+            name: "Ecommerce Website",
             description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, dolore! Vero, quam aliquam? Obcaecati labore laboriosam, repellendus officiis totam magni',
             link: 'learn more'
         },
         {
-            name: " UI / UX design",
+            name: "Search engine Optimization",
             description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, dolore! Vero, quam aliquam? Obcaecati labore laboriosam, repellendus officiis totam magni',
             link: 'learn more'
         }
@@ -30,25 +31,44 @@ const Service = () => {
         <div className="py-8 lg:py-24 lg:h-screen flex items-center text-white" id="service">
             <div className='container mx-auto'>
                 <div className='flex flex-col lg:flex-row'>
-                    <div className='flex-1 lg:bg-services lg:bg-bottom lg:bg-no-repeat mix-blend-lighten mb-12 lg:mb-0 '>
+                    <motion.div
+                        variants={fadeIn('down', 0.3)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }}
+                        className='flex-1 lg:bg-services lg:bg-bottom lg:bg-no-repeat mix-blend-lighten mb-12 lg:mb-0 '>
                         <h1 className='font-semibold text-4xl text-accent mb-6'>What i do</h1>
                         <h3 className='font-semibold text-lg max-w-[456px] mb-16'>Im a junior front end web developer with 1 years of experiance</h3>
                         <button className='btn p-2 rounded-lg'>Hire me </button>
 
-                    </div>
-                    <div>
+                    </motion.div>
+                    <motion.div
+                        variants={fadeIn('up', 0.3)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }}
+
+                        className='flex-1'>
                         {
 
-                            services.map(service, index)=>{
-                    return(
-                        <div key={index}>
-                            <div>text</div>
-                            <div>Link</div>
-                        </div>
-                        )
+                            services.map((service, index) => {
+                                const { name, description, link } = service
+                                return (
+
+                                    <div className='border-b border-white/20 h-[150px] mb-[38px] flex ' key={index}>
+                                        <div className='max-w-[476px]'>
+                                            <h4 className='text-[20px] tracking-winder font-primary font-semibold mb-6'>{name}</h4>
+                                            <p className='font-secendary leading-tight'>{description}</p>
+                                        </div>
+                                        <div className='flex flex-col flex-1 item-end'>
+                                            <a className='w-9 h-9 p-3 rounded-lg btn mb-[42px] flex-center items-center' href="#">
+                                                <FaLocationArrow></FaLocationArrow>
+                                            </a>
+                                            <a className='text-sm text-purple-600 font-semibold' href="#">{link}</a>
+
+                                        </div>
+
+                                    </div>
+                                )
+
+                            })
+
                         }
-                        }
-                    </div>
+                    </motion.div>
                 </div>
 
 
